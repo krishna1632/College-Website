534d30720096af9300b6020a6a10053d706ec86b
pra8953 <prabhatkumar6920@gmail.com>
2024-08-23 14:53:19 +0530

    image

--- RC-website/src/components/Hero.jsx
+++ RC-website/src/components/Hero.jsx
@@ -4,8 +4,8 @@ function Hero() {
   return <React.Fragment>
 
         <div>
-            <div className="w-full h-[679px] ">
-                <img className="w-full h-[700px]"src="Hero_images/download.jpg" alt="" />
+            <div className="w-full h-[679px] flex justify-center items-center ">
+                <img className="h-[679px] w-[100vw] bg-cover"src="Hero_images/download.jpg" alt="" />
             </div>
         </div>
 
