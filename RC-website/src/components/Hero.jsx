644bbc1bdd8b639ec5f57e3b906f0b0b9ab6fddb
pra8953 <prabhatkumar6920@gmail.com>
2024-08-23 13:38:28 +0530

    hero

---
+++ RC-website/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hero() {
+  return <React.Fragment>
+
+        <div>
+            <div className="w-full h-[679px] ">
+                <img className="w-full h-[700px]"src="Hero_images/download.jpg" alt="" />
+            </div>
+        </div>
+
+        </React.Fragment>;
+}
+
+export default Hero;
