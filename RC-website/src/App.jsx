325578f11b7c1d1804522243286bdad3c63598ae
pra8953 <prabhatkumar6920@gmail.com>
2024-08-27 15:59:58 +0530

    hgdfhgd

--- RC-website/src/App.jsx
+++ RC-website/src/App.jsx
@@ -1,14 +1,11 @@
 import TopHeader from "./components/TopHeader"
 import ContentBox from "./components/ContentBox";
 import Desk from "./components/Desk";
-<<<<<<< HEAD
+
 import Hero from "./components/Hero"
 
-
-
-=======
 import NewsEvents from "./components/NewsEvents";
->>>>>>> 35fe005678fa07103a494d15ecb063d07728c75d
+
 
 function App() {
 
@@ -16,8 +13,11 @@ function App() {
     <>
       <header>
         <TopHeader></TopHeader>
+
+        
         <Hero></Hero>
         <ContentBox></ContentBox>
+        
       </header>
       <main>
         <Desk></Desk>
