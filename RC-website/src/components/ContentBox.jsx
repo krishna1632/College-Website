3c462c1c1b34cd71f1b5d66702a8235d62cd1086
pra8953 <prabhatkumar6920@gmail.com>
2024-08-28 15:01:44 +0530

    frgh

--- RC-website/src/components/ContentBox.jsx
+++ RC-website/src/components/ContentBox.jsx
@@ -29,9 +29,9 @@ const ContentBox = () => {
             contentData.map(
               (item, index) => {
                 return (
-                  <div key={index} className="flex flex-col justify-start items-center pt-[45px] h-[270px] w-[300px] font-bold cursor-pointer text-[#000]" style={{ backgroundColor: item.color }}>
+                  <div key={index} className="flex flex-col justify-start items-center pt-[30px] h-[250px] w-[300px] font-bold cursor-pointer text-[#000]" style={{ backgroundColor: item.color }}>
                     <span>{item.name}</span>
-                    <img src={item.image} alt="" className="mt-2 h-[150px] w-auto" style={{maxHeight:`150px`}}/>
+                    <img src={item.image} alt="" className=" h-[150px] w-auto" style={{maxHeight:`150px`}}/>
                   </div>
                 )
               }
